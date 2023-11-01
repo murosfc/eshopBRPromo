0425ae0fa7ade9d2e78413c4793384f660b1e3ac
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2023-10-31 22:26:27 -0300

    Update UserServiceInterface.ts

--- DataBaseAPI/src/service/contract/UserServiceInterface.ts
+++ DataBaseAPI/src/service/contract/UserServiceInterface.ts
@@ -4,4 +4,5 @@ import { DomainServiceInterface } from "./DomainServiceInterface";
 export interface UserServiceInterface extends DomainServiceInterface<User> {
     findByEmail(email: string): Promise<User>;
     findByToken(token: string): Promise<User>;
+    authenticate(email: string, password: string): Promise<User>;
 }
