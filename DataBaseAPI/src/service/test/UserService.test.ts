077be328d9f8a9c507ddf7d6b03a6e63839d6a20
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2023-11-06 23:15:26 -0300

    adicionados testes de atualização de usuário

--- DataBaseAPI/src/service/test/UserService.test.ts
+++ DataBaseAPI/src/service/test/UserService.test.ts
@@ -22,13 +22,18 @@ describe("UserService", () => {
         repository.manager.connection.destroy
     })
 
-    it("should create a user and authenticate", async () => {         
+    it("should create a user, authenticate and find by token", async () => {         
         user = await sut.save(user) as User;             
         expect(user.id).toBeGreaterThan(0);
 
         const authUser = await sut.authenticate(user.email, rawUserPassword);        
         expect(authUser).toBeInstanceOf(User);
         expect((authUser as User).acessToken).not.toBeNull();
+        const token = (authUser as User).acessToken;
+
+        const userByToken = await sut.findByToken(token);
+        expect(userByToken).toBeInstanceOf(User);
+        expect((userByToken as User).id).toEqual(user.id);
     })
 
     it("should return Erro type InvalidAttributeException when trying to save a user with invalid email", async () => {
@@ -42,6 +47,23 @@ describe("UserService", () => {
         const result = await sut.save(invalidUser);        
         expect(result).toBeInstanceOf(InvalidAttributeException);
     })
+
+    it("should update a user e-mail", async () => {
+        var savedUser = await sut.save(new User("no@spam.com", "12345")) as User;
+        savedUser = await sut.authenticate(savedUser.email, savedUser.password) as User;
+        savedUser.email = "yes@spam.com";        
+        const updatedUser = await sut.update(savedUser, savedUser.acessToken) as User;        
+        expect(updatedUser.email).toEqual("yes@spam.com");
+    });
+
+    it("should update a user password", async () => {
+        var savedUser = await sut.save(new User("notwhilling@spam.com", "12345")) as User;
+        savedUser = await sut.authenticate(savedUser.email, savedUser.password) as User;        
+        savedUser.password = "SuperSecretPassword";     
+        const updatedUser = await sut.update(savedUser, savedUser.acessToken) as User;        
+        expect(updatedUser.updatedOn).not.toBeNull(); 
+        expect(savedUser.email).toEqual(updatedUser.email);       
+    });
 
     
 })
