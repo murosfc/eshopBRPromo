7b8873e517424f14890414f6cccf89596988e203
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2023-11-06 23:14:42 -0300

    Interface atualizada com autenticação

--- DataBaseAPI/src/service/contract/UserServiceInterface.ts
+++ DataBaseAPI/src/service/contract/UserServiceInterface.ts
@@ -1,7 +1,11 @@
 import { User } from "../../model/User";
-import { DomainServiceInterface } from "./DomainServiceInterface";
 
-export interface UserServiceInterface extends DomainServiceInterface<User> {
+export interface UserServiceInterface {
+    save(entity: User): Promise<User | Error>;
+    update(entity: User, token: string): Promise<User | Error>;
+    delete(entity: User, token: string): Promise<boolean>;
+    findById(id: number): Promise<User | Error>;
+    findAll(): Promise<User[]>;
     findByEmail(email: string): Promise<User | Error>;
     findByToken(token: string): Promise<User | Error>;
     authenticate(email: string, password: string): Promise<User | Error>;
